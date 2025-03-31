b85c4e9f525c619ddeed693a24a68031962d95eb
odaytelbany <oday0595814990@gmail.com>
2025-03-31 15:12:14 +0200

    Save content store locally

--- frontend/src/store/content.js
+++ frontend/src/store/content.js
@@ -1,6 +1,11 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useContentStore = create((set) => ({
+export const useContentStore = create(
+  persist((set) => ({
     contentType: "movie",
-    setContentType: (type) => set({contentType: type}),
-}))
+    setContentType: (type) => set({ contentType: type }),
+  }),
+  {name: "contentStore"}
+)
+);
